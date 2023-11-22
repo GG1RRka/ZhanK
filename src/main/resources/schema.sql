13ebbc49abf39f2267d49a136566b7f96c15eee6
Zhangir <zhanik0303@gmail.com>
2023-11-22 18:32:53 +0600

    Practise 6

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -27,3 +27,24 @@ CREATE TABLE timeslots (
                       id INT PRIMARY KEY,
                       starttime VARCHAR(255)
 );
+CREATE TABLE matches (
+    id INT PRIMARY KEY,
+    team1_id INT,
+    team2_id INT,
+    stadium_id INT,
+    timeslot_id INT,
+    match_date DATE
+);
+CREATE TABLE results (
+    match_id INT PRIMARY KEY,
+    team1_score INT,
+    team2_score INT,
+    winner_id INT
+);
+CREATE TABLE coaches (
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    surname VARCHAR(255),
+    team_id INT,
+    country VARCHAR(255)
+);
